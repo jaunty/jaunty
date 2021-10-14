f518d8bf4d973d356ff4235befcc898bc452b837
Max Reiter <max.m.reiter@gmail.com>
2021-10-14 00:58:46 -0700

    implement request factory into httpx

--- internal/pkg/httpx/helpers.go
+++ internal/pkg/httpx/helpers.go
@@ -1,1 +1,28 @@
 package httpx
+
+import "net/http"
+
+func Headers(strs ...string) http.Header {
+	if len(strs)%2 != 0 {
+		panic("httpx: uneven strings given")
+	}
+
+	h := http.Header{}
+	var skip bool
+	for i := 0; i < len(strs); i++ {
+		if i == len(strs) {
+			break
+		}
+
+		if skip {
+			skip = false
+			continue
+		}
+
+		x := i + 1
+		h.Set(strs[i], strs[x])
+		skip = true
+	}
+
+	return h
+}
--- internal/pkg/httpx/request.go
+++ internal/pkg/httpx/request.go
@@ -1,6 +1,7 @@
 package httpx
 
 import (
+	"context"
 	"io"
 	"net/http"
 	"net/url"
@@ -10,18 +11,63 @@ type requestOptions struct {
 	Headers http.Header
 	Query   url.Values
 	Body    io.Reader
+	Method  string
 }
 
 type RequestOption func(*requestOptions)
 
+// WithQuery sets a request's query parameters.
 func WithQuery(q url.Values) RequestOption {
 	return func(ro *requestOptions) {
 		ro.Query = q
 	}
 }
 
+// WithBody sets a request's body.
 func WithBody(r io.Reader) RequestOption {
 	return func(ro *requestOptions) {
 		ro.Body = r
 	}
+}
+
+// WithHeaders sets a request's headers.
+func WithHeaders(h http.Header) RequestOption {
+	return func(ro *requestOptions) {
+		ro.Headers = h
+	}
+}
+
+// WithMethod sets a request's method.
+func WithMethod(m string) RequestOption {
+	return func(ro *requestOptions) {
+		ro.Method = m
+	}
+}
+
+// NewRequest creates a new HTTP request with the given options.
+// If opts.Method is blank, the method is automatically set to GET.
+func NewRequest(ctx context.Context, url string, opts ...RequestOption) (*http.Request, error) {
+	var ro *requestOptions
+	for _, o := range opts {
+		o(ro)
+	}
+
+	if ro.Method == "" {
+		ro.Method = http.MethodGet
+	}
+
+	req, err := http.NewRequestWithContext(ctx, ro.Method, url, ro.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if ro.Headers != nil {
+		req.Header = ro.Headers
+	}
+
+	if ro.Query != nil {
+		req.URL.RawQuery = ro.Query.Encode()
+	}
+
+	return req, nil
 }
