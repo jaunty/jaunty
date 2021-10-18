9a21fdc3cfc43bd5e52b45599064bf88d8d4b602
Max Reiter <max.m.reiter@gmail.com>
2021-10-18 11:40:17 -0700

    add basic web server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jaunty/jaunty
 
 go 1.17
+
+require github.com/go-chi/chi/v5 v5.0.4
---
+++ internal/web/web.go
@@ -0,0 +1,50 @@
+package web
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/zikaeroh/ctxlog"
+	"go.uber.org/zap"
+)
+
+type Options struct {
+	Addr string
+}
+
+// Server is responsible for serving the website and auth server.
+type Server struct {
+	Addr string
+}
+
+// New creates a new Server.
+func New(opts *Options) (*Server, error) {
+	s := &Server{
+		Addr: opts.Addr,
+	}
+
+	return s, nil
+}
+
+func (s *Server) router() *chi.Mux {
+	r := chi.NewRouter()
+	return r
+}
+
+// Start runs a Server.
+func (s *Server) Start(ctx context.Context) error {
+	srv := &http.Server{
+		Handler: s.router(),
+		Addr:    s.Addr,
+	}
+
+	go func() {
+		if err := srv.Shutdown(context.Background()); err != nil {
+			ctxlog.Error(ctx, "error shutting down server", zap.Error(err))
+		}
+	}()
+
+	ctxlog.Info(ctx, "starting server", zap.String("addr", s.Addr))
+	return srv.ListenAndServe()
+}
